0c56f4ebe72e6e1a650d586a04597d1f91e86e41
marosirobi <marosirobi666@gmail.com>
2025-12-10 16:25:16 +0100

    demo-01

---
+++ 01-springboot-demo/myapp/src/main/java/com/example/springboot/demo/myapp/MyappApplication.java
@@ -0,0 +1,19 @@
+package com.example.springboot.demo.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class MyappApplication {
+
+	public static void main(String[] args) {
+
+
+	}
+
+
+
+
+}
